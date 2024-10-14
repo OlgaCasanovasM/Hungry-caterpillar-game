d7fa4881cb036eee16ba2b93f4a8779e932dcd58
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-14 11:52:44 +0200

    create caterpillar class + basic tunel element

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log("Caterpillar");
+class Caterpillar {
+  constructor() {
+    this.width = 20;
+    this.height = 40;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+  moveUp() {}
+  moveDown() {}
+}
