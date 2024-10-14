cb62a77fe0da63e15055d72846f36c69e0a339ac
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-14 13:44:20 +0200

    move caterpiller + listeners

--- js/index.js
+++ js/index.js
@@ -1,28 +1,44 @@
+////////////CLASSES///////////////
+
 class Caterpillar {
   constructor() {
     this.width = 20;
     this.height = 10;
     this.positionX = 20;
     this.positionY = 30;
+    this.caterpillar = null;
 
     this.displayCaterpillar();
   }
   displayCaterpillar() {
-    const caterpillar = document.createElement("div");
-    caterpillar.id = "caterpillar";
-    caterpillar.style.width = this.width + "vw";
-    caterpillar.style.height = this.height + "vh";
-    caterpillar.style.left = this.positionX + "vw";
-    caterpillar.style.bottom = this.positionY + "vh";
+    this.caterpillar = document.createElement("div");
+    this.caterpillar.id = "caterpillar";
+    this.caterpillar.style.width = this.width + "vw";
+    this.caterpillar.style.height = this.height + "vh";
+    this.caterpillar.style.left = this.positionX + "vw";
+    this.caterpillar.style.bottom = this.positionY + "vh";
     const tunnel = document.getElementById("tunnel");
-    tunnel.appendChild(caterpillar);
+    tunnel.appendChild(this.caterpillar);
   }
   moveUp() {
     this.positionY++;
+    this.caterpillar.style.bottom = this.positionY + "vh";
   }
   moveDown() {
     this.positionY--;
+    this.caterpillar.style.bottom = this.positionY + "vh";
   }
 }
 
-const caterpillar = new Caterpillar();
+///////////////////INSTANCES///////////////////////////
+const newPlayer = new Caterpillar();
+
+/////////////EVENT LISTENERS//////////////////////////
+
+document.addEventListener("keydown", function (element) {
+  if (element.code === "ArrowUp") {
+    newPlayer.moveUp();
+  } else if (element.code === "ArrowDown") {
+    newPlayer.moveDown();
+  }
+});
