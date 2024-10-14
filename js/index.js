97427ddc7b0980adc0c21efa6153d51ca5134e72
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-14 13:14:16 +0200

    display caterpillar dinamically

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 class Caterpillar {
   constructor() {
     this.width = 20;
-    this.height = 40;
-    this.positionX = 0;
-    this.positionY = 0;
+    this.height = 10;
+    this.positionX = 20;
+    this.positionY = 30;
+
+    this.displayCaterpillar();
   }
-  moveUp() {}
-  moveDown() {}
+  displayCaterpillar() {
+    const caterpillar = document.createElement("div");
+    caterpillar.id = "caterpillar";
+    caterpillar.style.width = this.width + "vw";
+    caterpillar.style.height = this.height + "vh";
+    caterpillar.style.left = this.positionX + "vw";
+    caterpillar.style.bottom = this.positionY + "vh";
+    const tunnel = document.getElementById("tunnel");
+    tunnel.appendChild(caterpillar);
+  }
+  moveUp() {
+    this.positionY++;
+  }
+  moveDown() {
+    this.positionY--;
+  }
 }
+
+const caterpillar = new Caterpillar();
